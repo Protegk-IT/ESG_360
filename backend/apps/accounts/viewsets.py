271d9a4e8a3199fde8c168461c194460efa3f84e
reshma-golandaj <reshmasgolandaj@gmail.com>
2026-08-07 16:21:51 +0530

    Added ScopePermissionMixin in RBACModelViewSet

--- backend/apps/accounts/viewsets.py
+++ backend/apps/accounts/viewsets.py
@@ -2,9 +2,10 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from apps.accounts.permissions import HasRolePermission
+from apps.accounts.mixins import ScopedPermissionMixin
 
 
-class RBACModelViewSet(viewsets.ModelViewSet):
+class RBACModelViewSet(ScopedPermissionMixin, viewsets.ModelViewSet):
     """
     Base ViewSet for RBAC protected APIs.
     """
