fcf09d6ad3236ee4135fd4b50fe2416b748fee5b
reshma-golandaj <reshmasgolandaj@gmail.com>
2026-08-08 18:14:21 +0530

    fix: expose core API routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('apps.accounts.urls')),
+    path("api/", include("apps.core.urls")),
     path('api/company/', include('apps.companies.urls')),
     path("api/org/", include("apps.organizations.urls")),
     path("api/periods/", include("apps.periods.urls"))
