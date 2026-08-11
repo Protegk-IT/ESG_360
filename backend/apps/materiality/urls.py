3a3765d73cdde8c53d5b8f8350bf81b0dfaec9d4
pratikraut002 <devpratik.raut@gmail.com>
2026-08-11 10:51:53 +0530

    fix assessments endpoint

--- backend/apps/materiality/urls.py
+++ backend/apps/materiality/urls.py
@@ -1,23 +1,22 @@
 
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (MaterialityAssessmentViewSet,TopicCategoryListCreateView,MaterialTopicListCreateView,MaterialSubTopicListCreateView,)
 
 
+app_name = 'materiality'
+router = DefaultRouter()
 
 
+router.register(r"assessments", MaterialityAssessmentViewSet,basename="materiality-assessment",)
 
-urlpatterns = [path("topics/categories/",TopicCategoryListCreateView.as_view(),name="topic-category-list-create",),
+urlpatterns = [
+               path('', include(router.urls)),
+               path("topics/categories/",TopicCategoryListCreateView.as_view(),name="topic-category-list-create",),
                path( "topics/", MaterialTopicListCreateView.as_view(),name="material-topic-list-create",),
                path("topics/subtopics/",MaterialSubTopicListCreateView.as_view(),name="material-subtopic-list-create",),
                ]
 
 
 
-### Material Assesment routers
-router = DefaultRouter()
-
-router.register(r"assessments", MaterialityAssessmentViewSet,basename="materiality-assessment",)
-
-urlpatterns = router.urls
