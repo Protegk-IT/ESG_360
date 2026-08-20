e7a9dd8cdafc1e3021d068f140255ae72a988a12
rajan12thakur <raajan12thakur@gmail.com>
2026-08-20 10:35:49 +0530

    all changes reflected in backend code

--- backend/apps/datapoints/admin.py
+++ backend/apps/datapoints/admin.py
@@ -7,13 +7,7 @@ from apps.datapoints.models import Datapoint, DatapointCategory, DatapointOption
 admin.site.register(UnitFamily)
 admin.site.register(Unit)
 admin.site.register(DatapointCategory)
-#admin.site.register(Datapoint)
-@admin.register(Datapoint)
-class DatapointAdmin(admin.ModelAdmin):
-    search_fields = (
-        "code",
-        "label",
-    )
+admin.site.register(Datapoint)
 admin.site.register(DatapointOption)
 admin.site.register(DatapointTableColumn)
 admin.site.register(DatapointTableRow)
